fc436f90fee77455b721657380f6583abbb69d3a
CJ3010406 <CJ3010406@LAB01-PC11>
2023-04-12 11:57:23 -0300

    Adicionar arquivos de projeto.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //     *****LISTA 5 *****
+
+
+
+            //xXEXERCICIO 1Xx
+
+
+            //Console.WriteLine("entre com n");
+            //int n=int.Parse(Console.ReadLine());
+            //for(int i = 1; i<=n ; i++)
+            //{
+            //    Console.WriteLine("VALOR DE N = "+i);   
+            //}
+            //Console.ReadKey     ();
+
+            //================================//================================//=========================
+
+            //xXEXERCICIO 2Xx
+
+            //Console.WriteLine("Entre com N");
+            //int n = int.Parse(Console.ReadLine());
+
+
+
+            //for (int i = 100; i >= n; i--)
+            //{
+
+
+            //    Console.WriteLine("os PARES n = " + i--);
+            //}
+            //Console.ReadKey();
+            //===============================//=========================//=========================
+
+            //xXEXERCICIO 3Xx
+
+            //Console.WriteLine("Entre com N");
+            //int n = int.Parse(Console.ReadLine());
+
+
+
+            //for (int i = 1000; i >= n; i--)
+            //{
+
+            //    if (i % 2 == 0)
+            //        Console.WriteLine("os PARES n = " + i--);
+            //}
+            //Console.ReadKey();
+
+            //=======================================//=========================================//===============
+
+            //  xX EXERCICIO 4 Xx
+
+            //Console.WriteLine("entre com um valor :");
+            //double soma = 0, numc4;
+            //for(; soma<200;)
+            //{
+            //    Console.WriteLine("Digite um numero inteiro: ");
+            //    numc4 = double.Parse(Console.ReadLine());
+
+            //    if (numc4 > 0)
+            //    {
+            //        soma += numc4;
+            //        Console.WriteLine("numero positivo* soma atual: " + soma);
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine("numero negativo ou zero");
+            //    }
+
+            //}
+            //Console.ReadKey();
+
+            // *** EXERCICIO 5 *****
+
+            //Console.Write("Digite um número inteiro: ");
+            //int num = int.Parse(Console.ReadLine());
+
+            //Console.WriteLine("Divisores de " + num + ":");
+
+            //for (int i = 1; i <= num; i++)
+            //{
+            //    if (num % i == 0)
+            //    {
+            //        Console.WriteLine(i);
+            //    }
+            //    Console.ReadKey();  
+            //}
+
+            // ****Exercicio 6**
+
+            //int numero;
+            //int maior = int.MinValue;
+            //int menor = int.MaxValue;
+
+            //for (int i = 0; i < 10; i++)
+            //{
+            //    Console.Write("Digite um número: ");
+            //    numero = int.Parse(Console.ReadLine());
+
+            //    if (numero > maior)
+            //    {
+            //        maior = numero;
+            //    }
+
+            //    if (numero < menor)
+            //    {
+            //        menor = numero;
+            //    }
+            //}
+
+            //Console.WriteLine("Maior número digitado: " + maior);
+            //Console.WriteLine("Menor número digitado: " + menor);
+
+            //Console.ReadKey()
+        }
+    }
+}
